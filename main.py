fb08a03f1bc640f55c7c44664aa66904c4c46db9
Atlas <gunduza@igmr.rwth-aachen.de>
2026-05-08 13:57:15 +0200

    Connect UI to backend: fix import path, CORS, MCP pipeline

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:5174",
         "http://localhost:3001",
     ],
     allow_credentials=True,
